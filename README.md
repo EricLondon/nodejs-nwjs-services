3bc7d9e1549a39476e9e161e199df7633f0850ba
Eric London <git@ericlondon.com>
2015-07-09 10:19:28 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+NOTE: this repo is a work in progress, very alpha :)
+
+To Run:
+
+```
+/Applications/nwjs-v0.12.2-osx-x64/nwjs.app/Contents/MacOS/nwjs .
+```
+
+TODO:
+- https://github.com/nwjs/nw.js/wiki/How-to-package-and-distribute-your-apps
+- alias psa="ps aux | egrep -i '(postgres|memcache|ruby|java|sidekiq|redis|nginx|mysql|elasticsearch)' | grep -iv grep"
---
+++ javascripts/app.js
@@ -0,0 +1,94 @@
+$(function() {
+
+  "use strict";
+
+  // debug window
+  require('nw.gui').Window.get().showDevTools();
+
+  // include node standard libraries
+  var fs = require("fs");
+
+  // include node npm packages
+  var promise = require('bluebird');
+
+  // add async/promise
+  promise.promisifyAll(fs);
+
+  //////////////////////////////////////////////////
+
+  var run_cmd = function(cmd, args) {
+    return new promise(function(resolve,reject){
+      var spawn = require('child_process').spawn;
+      var child = spawn(cmd, args);
+      var resp = "";
+
+      child.stdout.on('data', function (buffer) { resp += buffer.toString() });
+      child.stdout.on('end', function() { resolve (resp) });
+    });
+  }
+
+  var services = [];
+
+  promise.bind(services)
+    .then(function(){
+      return fs.readFileAsync('config/service_config.json', 'utf8');
+    })
+    .then(JSON.parse)
+    .then(function(parsed){
+      this.push.apply(this, parsed);
+    })
+    .then(function(){
+      return fs.readFileAsync('templates/services.hbs', 'utf8');
+    })
+    .then(function(template_source){
+      var template = Handlebars.compile(template_source);
+      $('[data-service-container]').append(template( { services: this } ));
+      return this;
+    })
+    .each(function(service) {
+
+      return run_cmd("ps", ["aux"]).then(function(output){
+
+        var output_lines = output.split(/\r?\n/);
+        var filtered = output_lines.filter(function(x){
+          var regex = new RegExp(service.ps_search, 'i');
+          return regex.test(x);
+        });
+
+        // update data
+        service.is_running = filtered.length > 0 ? true : false;
+
+      });
+
+    })
+    .each(function(service){
+
+      var $service_name_td = $('[data-service-container] [data-service-name="' + service.name + '"]');
+      var $service_status_td = $service_name_td.parent('tr').find('[data-service-status]');
+
+      // hide spans
+      $('span.label-success, span.label-danger', $service_status_td).hide();
+
+      // show status span
+      if (service.is_running) {
+        $('span.label-default', $service_status_td).hide();
+        $('span.label-success', $service_status_td).show();
+      }
+      else {
+        $('span.label-default', $service_status_td).hide();
+        $('span.label-danger', $service_status_td).show();
+      }
+
+    })
+    .then(function(){
+
+      // debug
+      //console.log(this);
+
+    })
+    .catch(function(e) {
+      //console.error("ERROR: ", JSON.stringify(e));
+      console.error("ERROR: ", e);
+    });
+
+});
